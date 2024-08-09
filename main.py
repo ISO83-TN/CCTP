bb45b72e9c552a0cb14ce6c936424c2b483cea90
ISO83-TN <oussamaismail83@gmail.com>
2024-08-09 12:17:08 +0000

    MAJ

--- main.py
+++ main.py
@@ -1,14 +1,1 @@
-from flask import Flask
-from project.routes import main as main_blueprint
-
-def create_app():
-    app = Flask(__name__)
-
-    # Enregistrer le blueprint
-    app.register_blueprint(main_blueprint)
-
-    return app
-
-if __name__ == '__main__':
-    app = create_app()
-    app.run(debug=True)
+print("hello")
